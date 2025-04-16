9c86ad1ea6d55c763a86bf2418fcfba9d462f3b5
Fidel Genre <trabajosfidel4@gmail.com>
2025-04-16 04:19:16 -0300

    api

--- index.js
+++ index.js
@@ -39,33 +39,6 @@ navLinks.forEach(link => {
     });
 });
 
-/* Form submission
-//const contactForm = document.getElementById('contactForm');
-
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // Get form values
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
-    
-    // Here you would typically send the form data to a server
-    // For this example, we'll just log it to the console
-    console.log({
-        name,
-        email,
-        subject,
-        message
-    });
-    
-    // Show success message (in a real application)
-    alert('Thank you for your message! I will get back to you soon.');
-    
-    // Reset form
-    contactForm.reset();
-});*/
 
 // Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
