67cf8d1486937c3d190a6a041c9887babb3a2fe6
Fidel <trabajosfidel4@gmail.com>
2026-01-15 05:59:14 -0300

    secondcv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -87,7 +87,7 @@ export default function About() {
 
             <div className={styles.ctaRow}>
               <a
-                href="https://drive.google.com/file/d/195P4BsVlApD7_4unvQWC68SNcnOSoXes/view?usp=sharing"
+                href="https://drive.google.com/file/d/1sNqVPn32rbyHrOuO0JeFGxROjz2wXkBp/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className={styles.viewCV}
